cc6bcb72b04fe4a6cd69e64d24e0e606e16cde26
Stephen Gladney <stephen@ordernerd.com>
2019-11-30 21:14:28 -0500

    first commit

---
+++ lib/identicon.ex
@@ -0,0 +1,26 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for Identicon.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Identicon.hello()
+      :world
+
+  """
+  def main(input) do
+    input
+    |> hash_input
+
+  end
+
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+    
+  end
+end
